4ee87e7b5daea72ebade7f08cf402eae03623d31
TriggerJames <Kimothojames254@gmail.com>
2024-10-31 14:24:31 +0300

    More Updates
    Updates

--- zing_it_frontend/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-// src/components/PrivateRoute.js
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children, isAuthenticated }) => {
-  return isAuthenticated ? children : <Navigate to="/" replace />;
-};
-
-export default PrivateRoute;
